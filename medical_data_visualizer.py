750da9c8f1bd01bc21f0d5646d78b48b56f35f82
Paulo Castro <pauloterrosocastro@gmail.com>
2025-06-20 17:55:44 +0000

    Finalizado

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -5,30 +5,44 @@ import numpy as np
 
 #Check
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+bmi = df['weight'] / ((df['height'] / 100) ** 2)
+df['overweight'] = (bmi > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = df.copy()
+    df_cat = pd.melt(
+        df_cat,
+        id_vars=['cardio'],  # manter esta coluna fixa
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
 
+    plot = sns.catplot(
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio',
+        kind='bar',
+        data=df_cat
+    )
 
 
     # 8
-    fig = None
+    fig = plot.fig
 
 
     # 9
@@ -39,21 +53,37 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df.copy()
+
+    #Cleaning 1
+    df_heat = df_heat[(df_heat['ap_lo'] <= df_heat['ap_hi']) &
+    (df_heat['height'] >= df_heat['height'].quantile(0.025)) &
+    (df_heat['height'] <= df_heat['height'].quantile(0.975)) &
+    (df_heat['weight'] >= df_heat['weight'].quantile(0.025)) &
+    (df_heat['weight'] <= df_heat['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr().round(1)
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 10))
 
     # 15
-
+    sns.heatmap(
+    corr,
+    mask=mask,             # Aplica a máscara à parte superior
+    annot=True,            # Mostra os valores dentro das células
+    fmt=".1f",             # Mostra os valores com uma casa decimal
+    center=0,              # Centro da escala de cores
+    square=True,           # Mantém as células quadradas
+    linewidths=0.5,        # Linhas de separação entre as células
+    cbar_kws={"shrink": 0.5}  # Ajusta o tamanho da barra lateral
+    )
 
 
     # 16
